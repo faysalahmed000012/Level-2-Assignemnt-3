2d19ac51460e919965b0d8b181ece689f15916bf
faysalahmed000012 <faysal000012@gmail.com>
2024-09-02 21:44:43 +0600

    netlify

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,10 @@ import router from "./app/routes";
 
 const app: Application = express();
 
-const allowedOrigins = ["https://level-2-assignment-5-client.vercel.app"];
+const allowedOrigins = [
+  "https://level-2-assignment-5-client.vercel.app",
+  "https://66d5dd1761a09e0992640ec4--exquisite-platypus-75cdf3.netlify.app/",
+];
 
 // parsers
 app.use(express.json());
