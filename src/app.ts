feacbd38744d0c02e7e5931756e6441a56377b98
faysalahmed000012 <faysal000012@gmail.com>
2024-09-02 21:48:12 +0600

    new format

--- src/app.ts
+++ src/app.ts
@@ -17,16 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps, curl requests, etc.)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    origin: ["https://level-2-assignment-5-client.vercel.app"],
     credentials: true,
   })
 );
