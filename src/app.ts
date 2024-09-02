495d9ef3b992353f7e89b166a6c72f9adc8570b9
faysalahmed000012 <faysal000012@gmail.com>
2024-09-02 22:29:06 +0600

    added /

--- src/app.ts
+++ src/app.ts
@@ -7,17 +7,12 @@ import router from "./app/routes";
 
 const app: Application = express();
 
-const allowedOrigins = [
-  "https://level-2-assignment-5-client.vercel.app",
-  "https://66d5dd1761a09e0992640ec4--exquisite-platypus-75cdf3.netlify.app/",
-];
-
 // parsers
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://level-2-assignment-5-client.vercel.app"],
+    origin: ["https://level-2-assignment-5-client.vercel.app/"],
     credentials: true,
   })
 );
